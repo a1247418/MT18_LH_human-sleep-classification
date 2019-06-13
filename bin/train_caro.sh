727c5bea9380c1a604212d7e96b4ec81fa0ef6bd
Lorenz Linhardt <l.linhardt@outlook.com>
2019-06-13 15:46:11 +0200

    Updated train_caro script

--- bin/train_caro.sh
+++ bin/train_caro.sh
@@ -19,7 +19,7 @@ if [ "${setup}" = "dssm_supervised" ]; then
     data_config=caro_all_2D_onesided_no_sweat
     model=DSSM_caro
     train_experts=false
-    unsupervized=false
+    unsupervized=False
 fi
 if [ "${setup}" = "dssm" ]; then
     data_config=caro_all_2D_onesided_no_sweat
@@ -52,7 +52,7 @@ if ${train_experts} ; then
             bsub -n 5 -W 4:00 -R "rusage[mem=6000,scratch=65000,ngpus_excl_p=1]" -K python train.py with caro_EMG_2D singlechanexp save_model=True ds.fold=${i} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${val_csv} log_dir=${log_dir}EMG&
         fi
         if [ "${setup}" = "dssm" ]; then
-           bsub -n 5 -W 10:00 -R "rusage[mem=18000,scratch=60000,ngpus_excl_p=1]" -K python train.py with ${data_config} DSSM_caro save_model=True unsupervized=${unsupervized} ds.fold=${i} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${val_csv} unsupervized=True log_dir=${log_dir}${setup}&
+           bsub -n 5 -W 10:00 -R "rusage[mem=8000,scratch=60000,ngpus_excl_p=1]" -K python train.py with ${data_config} DSSM_caro save_model=True unsupervized=${unsupervized} ds.fold=${i} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${val_csv} unsupervized=True log_dir=${log_dir}${setup}&
         fi
     done
     wait
@@ -62,14 +62,14 @@ fi
 if ${train} ; then
     for (( i=0; i<${n_folds}; i++ ))
      do
-        bsub -n 5 -W 10:00 -R "rusage[mem=18000,scratch=60000,ngpus_excl_p=1]" -K python train.py with ${data_config} ${model} save_model=True unsupervized=${unsupervized} ds.fold=${i} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${val_csv} log_dir=${log_dir}${setup}&
+        bsub -n 5 -W 10:00 -R "rusage[mem=8000,scratch=60000,ngpus_excl_p=1]" -K python train.py with ${data_config} ${model} save_model=True unsupervized=${unsupervized} ds.fold=${i} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${val_csv} log_dir=${log_dir}${setup}&
     done
     wait
 fi
 
 # Evaluate (cross validation)
 if ${cv} ; then
-    bsub -n 5 -W 10:00 -R "rusage[mem=18000,scratch=60000,ngpus_excl_p=1]" -K python cv.py with ${data_config} ${model} save_model=True unsupervized=${unsupervized} ds.nfolds=${n_folds} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${test_csv} log_dir=${log_dir}${setup}&
+    bsub -n 5 -W 10:00 -R "rusage[mem=8000,scratch=60000,ngpus_excl_p=1]" -K python cv.py with ${data_config} ${model} save_model=True unsupervized=${unsupervized} ds.nfolds=${n_folds} ds.data_dir=${data_path} ds.train_csv=${train_csv} ds.val_csv=${test_csv} log_dir=${log_dir}${setup}&
     wait
 fi
 
@@ -78,7 +78,7 @@ if ${evaluate} ; then
     eval_file="${log_dir}{setup}/model_best.pth.tar"
     for (( i=0; i<${n_folds}; i++ ))
     do
-        bsub -n 5 -W 4:00 -R "rusage[mem=18000,scratch=60000,ngpus_excl_p=1]" -K python validate.py --model=${eval_file} --data_dir=${data_dir} --subject_csv=${test_csv} --output_dir=${out_dir}&
+        bsub -n 5 -W 4:00 -R "rusage[mem=8000,scratch=60000,ngpus_excl_p=1]" -K python validate.py --model=${eval_file} --data_dir=${data_dir} --subject_csv=${test_csv} --output_dir=${out_dir}&
     done
     wait
 fi
